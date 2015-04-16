61034bf5e0a450f90370e8987703395bd7c893d7
william <williamwidmerr@gmail.com>
2015-04-16 16:55:00 -0400

    Update

--- mychown.c
+++ mychown.c
@@ -11,7 +11,7 @@
    C program demonstrating chown command.
    
    This program accepts one command line argument. A file. 
-   The file has its ownership changed to the current user and current group. Compile with
+   The file has its ownership changed to the root user (0).
    cc -o mychown mychown.c 
 
  */
@@ -19,12 +19,13 @@
 int main(int argc, char *argv[]){
 
   if (argc !=2){
-    printf("Improper use");
+    printf("Improper use\n");
     exit(1);
   }
   int userid = getuid();
   int groupid = getgid();
-  chown(argv[1], userid, groupid);
+  //  chown(argv[1], userid, groupid);
+  chown(argv[1], 0,0);
   exit(0);
   return 0;
 }
